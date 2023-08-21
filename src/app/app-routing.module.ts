6cea66642ad2d4386ee017a51fa4c123a9ea462b
shakir <mdshakir707@gmail.com>
2023-08-21 13:04:05 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AssessmentComponent } from './pages/assessment/assessment.component';
 
 const routes: Routes = [
-  // { path: 'pages/assessment', component: AssessmentComponent }
   {path:'',redirectTo:'/pages/assessment',pathMatch:'full'}
 ];
 
